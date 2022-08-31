9e2cea0ab9e440adecdba080ebbd0a464f4da08b
Frosti Grétarssn <frosti@landspitali.is>
2022-08-31 11:21:57 +0000

    change to tokio

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,28 @@
-use std::sync::RwLock;
+use tokio::sync::RwLock;
 use std::sync::Arc;
-use std::time::Duration;
-use std::time::Instant;
+use std::time::{Instant, Duration};
 
-struct CacheData<T, S>
+
+struct AsyncCacheData<T, S>
 {
     data: Arc<T>,
-    ttl: u128,
+    ttl: Duration,
     state: Option<S>,
     age: std::time::Instant,
     refresher: Box<dyn Fn(Option<&mut S>) -> T + Send + Sync>
 }
 
-impl<T, S> CacheData<T, S>
+impl<T, S> AsyncCacheData<T, S>
 {
     fn get_ref(&self) -> Result<Arc<T>, ()> {
-        if self.age.elapsed().as_millis() > self.ttl {
+        if self.age.elapsed() > self.ttl {
             return Err(());
         }
         Ok(Arc::clone(&self.data))
     }
 
     fn refresh(&mut self) -> Arc<T> {
-        if self.age.elapsed().as_millis() < self.ttl { 
+        if self.age.elapsed() < self.ttl { 
             return Arc::clone(&self.data);
         }
 
@@ -41,49 +41,41 @@ impl<T, S> CacheData<T, S>
     }
 }
 
-pub struct Cache<T, S>
+pub struct AsyncCache<T, S>
 {
-    data: RwLock<CacheData<T, S>>
+    data: RwLock<AsyncCacheData<T, S>>
 }
 
-pub type ACache<T, S> = Arc<Cache<T, S>>;
-
-impl<T, S> Cache<T, S>
+impl<T, S> AsyncCache<T, S>
 {
-    pub fn new(mut state: Option<S>, ttl: u128, refresher: Box<dyn Fn(Option<&mut S>) -> T + Send + Sync>) -> ACache<T, S> {
+    pub fn new(mut state: Option<S>, ttl: Duration, refresher: Box<dyn Fn(Option<&mut S>) -> T + Send + Sync>)
+        -> Self {
         let data = match state.as_mut() {
             Some(state) => refresher(Some(state)),
             None => refresher(None)
         };
-        let cd = CacheData {
+        let cd = AsyncCacheData {
             data: Arc::new(data),
             age: Instant::now(),
             state,
             ttl,
             refresher
         };
-        Arc::new(
-            Self {
-                data: RwLock::new(cd)
-            }
-        )
+        AsyncCache {
+            data: RwLock::new(cd)
+        }
     }
 
-    pub fn get_data(&self) -> Arc<T> {
+    pub async fn get_data(&self) -> Arc<T> {
         {
-            std::thread::sleep(Duration::from_secs(1));
-            let data = self.data.read().unwrap();
+            let data = self.data.read().await;
             if let Ok(data) = data.get_ref() {
                 return data;
             }
         }
         // Refresh if data is old
-        let mut data = self.data.write().unwrap();
+        let mut data = self.data.write().await;
         data.refresh()
-    }
-
-    pub fn clone(cache: &ACache<T, S>) -> ACache<T, S> {
-        Arc::clone(cache)
     }
 }
 
@@ -91,63 +83,63 @@ impl<T, S> Cache<T, S>
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::thread;
     use std::time::Duration;
+    use tokio::time::sleep;
 
-    #[test]
-    fn cache_test() {
+    #[tokio::test(flavor = "multi_thread")]
+    async fn cache_test() {
         struct State {
             i: u32
         }
         let s = State { i: 0 }; 
 
 
-        let cache: ACache<u32, State> = Cache::new(Some(s), 50, Box::new(|s| {
+        let cache: ACache<u32, State> = Cache::new(Some(s), Duration::from_millis(50), Box::new(|s| {
             let s = s.unwrap();
             s.i += 1;
             s.i
         }));
         
         let cache1 = Cache::clone(&cache);
-        let t1 = thread::spawn(move || {
-            let mut data = cache1.get_data();
+        let t1 = tokio::spawn( async move {
+            let mut data = cache1.get_data().await;
             assert_eq!(*data, 1);
-            thread::sleep(Duration::from_millis(10));
-            data = cache1.get_data();
+            sleep(Duration::from_millis(10)).await;
+            data = cache1.get_data().await;
             assert_eq!(*data, 1);
-            thread::sleep(Duration::from_millis(50));
-            data = cache1.get_data();
+            sleep(Duration::from_millis(50)).await;
+            data = cache1.get_data().await;
             assert_eq!(*data, 2);
-            thread::sleep(Duration::from_millis(20));
-            data = cache1.get_data();
+            sleep(Duration::from_millis(20)).await;
+            data = cache1.get_data().await;
             assert_eq!(*data, 2);
-            thread::sleep(Duration::from_millis(50));
-            data = cache1.get_data();
+            sleep(Duration::from_millis(50)).await;
+            data = cache1.get_data().await;
             assert_eq!(*data, 3);
         });
 
         let cache2 = Cache::clone(&cache);
-        let t2 = thread::spawn(move || {
-            let mut data = cache2.get_data();
+        let t2 = tokio::spawn( async move {
+            let mut data = cache2.get_data().await;
             assert_eq!(*data, 1);
-            thread::sleep(Duration::from_millis(20));
-            data = cache2.get_data();
+            sleep(Duration::from_millis(20)).await;
+            data = cache2.get_data().await;
             assert_eq!(*data, 1);
-            thread::sleep(Duration::from_millis(40));
-            data = cache2.get_data();
+            sleep(Duration::from_millis(40)).await;
+            data = cache2.get_data().await;
             assert_eq!(*data, 2);
-            thread::sleep(Duration::from_millis(10));
-            data = cache2.get_data();
+            sleep(Duration::from_millis(10)).await;
+            data = cache2.get_data().await;
             assert_eq!(*data, 2);
-            thread::sleep(Duration::from_millis(50));
-            data = cache2.get_data();
+            sleep(Duration::from_millis(50)).await;
+            data = cache2.get_data().await;
             assert_eq!(*data, 3);
 
         });
 
 
-        t1.join().unwrap();
-        t2.join().unwrap();
+        t1.await.unwrap();
+        t2.await.unwrap();
     }
 }
 
